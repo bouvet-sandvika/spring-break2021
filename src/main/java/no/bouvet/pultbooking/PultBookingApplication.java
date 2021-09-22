97697c8efcb4ba3ff91d6b20124f54076fd74bac
Daniel Idris <dai@spk.no>
2021-09-22 14:24:30 +0200

    Initial commit

---
+++ src/main/java/no/bouvet/pultbooking/PultBookingApplication.java
@@ -0,0 +1,13 @@
+package no.bouvet.pultbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PultBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PultBookingApplication.class, args);
+	}
+
+}
