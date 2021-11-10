2b0bd8719e965364f55e115d7eeb23ca255fd396
Daniel Idris <dai@spk.no>
2021-11-10 16:42:13 +0100

    InMemoryAuthentication with Roles

--- src/main/java/no/bouvet/deskbooking/configuration/SecurityConfig.java
+++ src/main/java/no/bouvet/deskbooking/configuration/SecurityConfig.java
@@ -1,12 +1,34 @@
 package no.bouvet.deskbooking.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password(passwordEncoder.encode("password"))
+                .roles("ADMIN")
+                .and()
+                .withUser("user2").password(passwordEncoder.encode("password"))
+                .roles();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -21,5 +43,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic();
 
     }
-
 }
--- src/main/java/no/bouvet/deskbooking/controller/DeskController.java
+++ src/main/java/no/bouvet/deskbooking/controller/DeskController.java
@@ -1,12 +1,19 @@
 package no.bouvet.deskbooking.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.annotation.security.RolesAllowed;
+
 import no.bouvet.deskbooking.models.dtos.DeskDto;
 import no.bouvet.deskbooking.models.entities.Desk;
 import no.bouvet.deskbooking.services.DeskService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/desks")
@@ -32,6 +39,7 @@ public class DeskController {
     }
 
     @PostMapping
+    @RolesAllowed({"ROLE_ADMIN"})
     public DeskDto saveDesk(@RequestBody DeskDto deskDto) {
         Desk desk = deskService.saveDesk(deskDto);
         return new DeskDto(desk.getId(), desk.getOffice().getId(), desk.getDeskLabel());
