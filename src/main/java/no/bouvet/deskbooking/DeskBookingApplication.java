8e931910d3ba9aeecd41909140d7455b3c871a3d
Knut Erik Skare <knut.skare@bouvet.no>
2021-11-13 11:10:14 +0100

    siste tillegg

--- src/main/java/no/bouvet/deskbooking/DeskBookingApplication.java
+++ src/main/java/no/bouvet/deskbooking/DeskBookingApplication.java
@@ -1,13 +1,27 @@
 package no.bouvet.deskbooking;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DeskBookingApplication {
 
+	Logger logger = LoggerFactory.getLogger(DeskBookingApplication.class);
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(DeskBookingApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo() {
+		return (args) -> {
+			logger.info("Hello world!");
+		};
 	}
 
 }
